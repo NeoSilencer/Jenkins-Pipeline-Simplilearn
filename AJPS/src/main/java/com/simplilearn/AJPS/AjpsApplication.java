79aff4e0310043ab2626e52c27a182735fe0092b
student <krish_acu@hotmail.com>
2021-02-23 14:09:48 +0000

    First commit Maven Project

---
+++ AJPS/src/main/java/com/simplilearn/AJPS/AjpsApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearn.AJPS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AjpsApplication.class, args);
+	}
+
+}
---
+++ AJPS/src/test/java/com/simplilearn/AJPS/AjpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn.AJPS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
